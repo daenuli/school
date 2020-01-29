e061b162d696560082518c52521709541104b254
masuryo <suryowidiyantgm@gmail.com>
2020-01-29 17:10:25 +0700

    add student migration

--- database/migrations/2020_01_23_020759_create_students_table.php
+++ database/migrations/2020_01_23_020759_create_students_table.php
@@ -15,12 +15,26 @@ class CreateStudentsTable extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('nis')->unique();
-            $table->string('name');
-            $table->string('birth_place');
-            $table->date('birth_date');
+            $table->string('nis', 6)->unique();
+            $table->string('name', 30);
+            $table->string('birth_place', 20);
+            $table->date('birth_date', 10);
+            $table->string('avatar', 200)->nullable();
+            $table->string('email', 100)->unique();
+            $table->string('phone', 15);
+            $table->boolean('gender'); //1 = Laki Laki, 2 = Perempuan
+            $table->boolean('status'); //1 = Aktif, 2 = Tidak Aktif
             $table->date('registration_date');
+            $table->unsigned('provinsi_id');
+            $table->unsigned('kabupaten_id');
+            $table->unsigned('kecamatan_id');
+            $table->string('father_name');
+            $table->string('mother_name');
             $table->timestamps();
+
+            $table->foreign('provinsi_id')->references('id')->on('provinsi')->onDelete('cascade');
+            $table->foreign('kabupaten_id')->references('id')->on('kabupaten')->onDelete('cascade');
+            $table->foreign('kecamatan_id')->references('id')->on('kecamatan')->onDelete('cascade');
         });
     }
 
