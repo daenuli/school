ce93eb5660b64ff4051f9f9ae6fecf8c77814c99
Nuli <giarsyani.nuli@gmail.com>
2020-02-01 15:50:08 +0700

    update laravel form builder template

---
+++ resources/views/vendor/laravel-form-builder/text.php
@@ -0,0 +1,27 @@
+<?php if ($showLabel && $showField): ?>
+    <?php if ($options['wrapper'] !== false): ?>
+    <div <?= $options['wrapperAttrs'] ?> >
+    <?php endif; ?>
+<?php endif; ?>
+
+<?php if ($showLabel && $options['label'] !== false && $options['label_show']): ?>
+    <?= Form::customLabel($name, $options['label'], $options['label_attr']) ?>
+<?php endif; ?>
+
+<?php if ($showField): ?>
+<div class="col-md-8">
+    <div class="form-group has-default">
+    <?= Form::input($type, $name, $options['value'], $options['attr']) ?>
+
+    <?php include 'help_block.php' ?>
+    </div>
+</div>
+<?php endif; ?>
+
+<?php include 'errors.php' ?>
+
+<?php if ($showLabel && $showField): ?>
+    <?php if ($options['wrapper'] !== false): ?>
+    </div>
+    <?php endif; ?>
+<?php endif; ?>
