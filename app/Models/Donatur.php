3e280900014a2cdd65a27e040bcffccd0b384a95
adibest <ahmadjayadibya@gmail.com>
2020-02-28 20:49:50 +0700

    detail, create, delete DonaturStudent

--- app/Models/Donatur.php
+++ app/Models/Donatur.php
@@ -7,4 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 class Donatur extends Model
 {
     protected $fillable = ['id','nik','name','date_birth','email','provinsi_id','kabupaten_id','kecamatan_id','street'];
+
+    public function provinsi()
+    {
+      return $this->belongsTo(Provinsi::class);
+    }
+
+    public function kabupaten()
+    {
+      return $this->belongsTo(Kabupaten::class);
+    }
+
+    public function kecamatan()
+    {
+      return $this->belongsTo(Kecamatan::class);
+    }
 }
